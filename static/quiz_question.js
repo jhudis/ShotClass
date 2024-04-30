3b0645ef4d882f4c8353d56a3cb2d8491d1d3e74
Aimee Oh <aoh705@yahooc.om>
2024-04-30 14:33:38 -0400

    changed quiz page and added proper functionaltiy in clicking buttons for answer

--- static/quiz_question.js
+++ static/quiz_question.js
@@ -1,33 +1,11 @@
-function display_ans(ans_true, ans_id, details){
-    if(ans_true == true){
-        var answer_ops = ``
-        for(let i = 0; i < details.options.length; i++){
-            let option = details.options[i]
-            var html_ops = ``
-            if(i == ans_id){
-                html_ops = `<button class="btn btn-primary" id="true-btn" data-id='"${i}"'>${option}</button>`
-            }
-            else{
-                html_ops = `<button class="btn btn-primary" id="option-btn" data-id='"${i}"'>${option}</button>`
-            }
-            answer_ops += html_ops
-        }
-        $('#clarification').empty()
+function display_ans(ans_true, ans, details, button){
+    if(ans_true == false){
+        $(button).css('background-color', 'red')
+
+        $('#clarification').html(`<span id='error-clarification'>${details.clarification}`)
     }
     else{
-        var answer_ops = ``
-        for(let i = 0; i < details.options.length; i++){
-            let option = details.options[i]
-            var html_ops = ``
-            if(i == ans_id){
-                html_ops = `<button class="btn btn-primary" id="false-btn" data-id='"${i}"'>${option}</button>`
-            }
-            else{
-                html_ops = `<button class="btn btn-primary" id="option-btn" data-id='"${i}"'>${option}</button>`
-            }
-            answer_ops += html_ops
-        }
-        $('#clarification').html(`<span id='error-clarification'>${details.clarification}`)
+        $(button).css('background-color', 'green');
     }
     var html_elem = `
             <div class="row">
@@ -44,8 +22,8 @@ function display_ans(ans_true, ans_id, details){
     $('#question').html(html_elem)
 }
 
-function change_user_stats(question_id, options_id){
-    let user_score = {"id": question_id, "answer_id": options_id}
+function change_user_stats(id, value, button){
+    let user_score = {"id": id, "answer": value}
     $.ajax({
         type: "POST",
         url: "change_score",                
@@ -55,9 +33,9 @@ function change_user_stats(question_id, options_id){
         success: function(result){
             let all_data = result["data"]
             ans_true = all_data["ans_true"]
-            given_ans_id = all_data["ans_id"]
+            given_ans = all_data["ans"]
             console.log(ans_true)
-            display_ans(ans_true, given_ans_id, details)
+            display_ans(ans_true, given_ans, details, button)
         },
         error: function(request, status, error){
             console.log("Error");
@@ -68,6 +46,57 @@ function change_user_stats(question_id, options_id){
     });
 }
 
+function check_ans(){
+    $('#ans-fs').click(function(){
+        var button = '#ans-fs'
+        var value = $(this).text()
+        console.log(value);
+        change_user_stats(id, value, button)
+    });
+
+    $('#ans-mfs').click(function(){
+        var button = '#ans-mfs'
+        var value = $(this).text()
+        console.log(value);
+        change_user_stats(id, value, button)
+    });
+
+    $('#ans-cs').click(function(){
+        var button = '#ans-cs'
+        var value = $(this).text()
+        console.log(value);
+        change_user_stats(id, value, button)
+    });
+
+    $('#ans-ms').click(function(){
+        var button = '#ans-ms'
+        var value = $(this).text()
+        console.log(value);
+        change_user_stats(id, value, button)
+    });
+
+    $('#ans-mcu').click(function(){
+        var button = '#ans-mcu'
+        var value = $(this).text()
+        console.log(value);
+        change_user_stats(id, value, button)
+    });
+
+    $('#ans-cu').click(function(){
+        var button = '#ans-cu'
+        var value = $(this).text()
+        console.log(value);
+        change_user_stats(id, value, button)
+    });
+
+    $('#ans-ecu').click(function(){
+        var button = '#ans-ecu'
+        var value = $(this).text()
+        console.log(value);
+        change_user_stats(id, value, button)
+    });
+}
+
 $(document).ready(function(){
     $('#qstn-title').html(`<div id='qstn-title' class="row"><b>Question ${id}</div>`)
 
@@ -75,25 +104,20 @@ $(document).ready(function(){
         <div class="row">
             <div id='qstn-qstn'>${details.question}</div>
         </div><br>
-        <div class="row">
-            <img src='${details.picture}'>
+        <div class="row" id='img-row'>
+            <img id='quiz-img' src='${details.picture}'>
         </div><br>
         `
     
     $('#question').html(html_elem)
 
-    $('#option-btn').click(function(){
-        let index = $(this).data("id")
-        options_id = index.replace(/[^0-9]/g,'')
-        console.log(options_id)
-        change_user_stats(id, options_id)
-    });
+    check_ans()
 
     var back_id = id - 1
     var forward_id = id + 1
 
     if(id > 1){
-        $('#quiz-btn-back').html(`<a href='/quiz/${back_id}'><button class="btn btn-primary">Back</button></a>`)
+        $('#quiz-btn-back').html(`<a href='/quiz/${back_id}'><button class="btn">Back</button></a>`)
     }
-    $('#quiz-btn-forward').html(`<a href='/quiz/${forward_id}'><button class="btn btn-primary">Next</button></a>`)
+    $('#quiz-btn-forward').html(`<a href='/quiz/${forward_id}'><button class="btn">Next</button></a>`)
 });
